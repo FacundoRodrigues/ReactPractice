ea30f6de5e080f60b57537a28de7176692d7cbfb
Facundo Rodrigues <facundo.rodrigues@urbetrack.com>
2023-09-10 19:35:31 -0300

    Agrega proyecto counter-app

---
+++ counter-app/src/CounterApp.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const CounterApp = ({value}) => {
+    
+    const [counter, setCounter] = useState(value);
+
+    const increment = () => setCounter(counter + 1)
+    
+    const decrement = () => setCounter(counter -1)
+
+    const reset = () => setCounter(value)
+
+    return (
+    <>
+        <h1>CounterApp</h1>
+        <h2>{counter}</h2>
+        <button onClick={ increment }>+1</button>
+        <button onClick={ decrement }>-1</button>
+        <button onClick={ reset }>Reset</button>
+    </>)
+};
+
+CounterApp.propTypes = {
+    value: PropTypes.number
+};
+
+CounterApp.defaultProps = {
+    value: 0
+};
+
+export default CounterApp;
