94f12eef9cc0329e39ff456c2a85d276a7f3d1fa
Facundo Rodrigues <facundo.rodrigues@urbetrack.com>
2023-09-19 18:06:55 -0300

    19-9-23: agrega AddCategory, GifGrid y GifGridItem

--- git-expert-app/src/GitExpertApp.js
+++ git-expert-app/src/GitExpertApp.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import { React, useState } from 'react'
+import AddCategory from './components/AddCategory'
+import { GifGrid } from './components/GifGrid'
 
 export const GitExpertApp = () => {
+    const [categories, setCategories] = useState([''])
+
     return (
         <>
             <h2>GitExpertApp</h2>
+            <AddCategory setcategories={ setCategories } />
             <hr />
+
+            <ol>
+                {
+                    categories.map( category => 
+                    <GifGrid
+                        category={ category }
+                        key={ category }
+                    />
+                    )
+                }
+            </ol>
         </>
     )
 }
