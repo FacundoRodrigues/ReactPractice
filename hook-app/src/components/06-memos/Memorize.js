e880842741f6111830b452da1fe27e104506c706
Facundo Rodrigues <facundo.rodrigues@urbetrack.com>
2023-10-03 09:09:48 -0300

    3-10-23: uso de React.memo

--- hook-app/src/components/06-memos/Memorize.js
+++ hook-app/src/components/06-memos/Memorize.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useCounter } from '../../hooks/useCounter'
+import { Small } from './Small'
 
 export const Memorize = () => {
     const { state: counter, increment } = useCounter(10)
-
+    const [ show, setShow ] = useState(true)
+    
     return (
         <>
             <h1>Memorize</h1>
-            <h3>Counter: {counter}</h3>
+            <h3>Counter: <Small value={counter} /> </h3>
 
             <hr />
 
             <button onClick={ () => increment(1) }>+1</button>
+
+            <button onClick={ () => { setShow(!show) } }>
+                Show/Hide {JSON.stringify(show)}
+            </button>
         </>
     )
 }
