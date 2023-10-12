b608c3260a736b697853429ac5489d9cee068b51
Facundo Rodrigues <facundo.rodrigues@urbetrack.com>
2023-10-12 19:29:59 -0300

    12-10-23: example react-router-dom

---
+++ hook-app/src/components/09-useContext/AppRouter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const AppRouter = () => {
+    return (
+        <h1>
+            <Link to="/HomeScreen">HomeScreen</Link>
+        </h1>
+    )
+}
--- hook-app/src/index.js
+++ hook-app/src/index.js
@@ -14,9 +14,20 @@ import './index.css';
 // import { MemoHook } from './components/06-memos/MemoHook';
 // import { CallbackHook } from './components/06-memos/CallbackHook';
 // import { Padre } from './components/07-tarea-memo/Padre';
-import { TodoApp } from './components/08-useReducer/TodoApp';
+// import { TodoApp } from './components/08-useReducer/TodoApp';
+import { MainApp } from './components/09-useContext/MainApp';
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: (
+            <MainApp />
+        )
+    }
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <TodoApp />
-);
+    <RouterProvider router={router} />
+)
