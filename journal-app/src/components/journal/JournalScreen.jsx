4260885e444eff2edde4a04e21724c0aaff93b37
Facundo Rodrigues <facundo.rodrigues@urbetrack.com>
2024-03-27 16:35:08 -0300

    27-3-24: commit pendiente

--- journal-app/src/components/journal/JournalScreen.jsx
+++ journal-app/src/components/journal/JournalScreen.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 
 export const JournalScreen = () => {
   return (
-    <h1 className='mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl'>JournalScreen</h1>
+    <>
+    <div className='bg-amber-800'>
+      <div className='container bg-blue-400 mx-auto'>
+        <h1 className='mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl'>JournalScreen</h1>
+        <p>asdasdasds</p>
+        <p>asdasdasds</p>
+        <p>asdasdasds</p>
+        <p>asdasdasds</p>
+        <p>asdasdasds</p>
+        <p>asdasdasds</p>
+        <p>asdasdasds</p>
+        <p>asdasdasds</p>
+        <p>asdasdasds</p>
+      </div>
+    </div>
+    </>
   )
 }
--- journal-app/src/main.jsx
+++ journal-app/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { JournalApp } from './JournalApp'
 
+import './index.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <JournalApp />
 )
